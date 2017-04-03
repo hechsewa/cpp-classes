ced5b1d7fc75ed517832ef2c83e3241cf0e389a7
Ewa Hechsman <ewa.hechsman@o2.pl>
2017-04-03 22:47:56 +0200

    textpool fix

--- lab5/textpool/TextPool.h
+++ lab5/textpool/TextPool.h
@@ -5,12 +5,35 @@
 #ifndef JIMP_EXERCISES_TEXTPOOL_H
 #define JIMP_EXERCISES_TEXTPOOL_H
 
+#include <initializer_list>
+#include <string>
+#include <algorithm>
+#include <cstring>
+#include <experimental/string_view>
+#include <set>
 
+using ::std::swap;
 
-class TextPool {
+namespace pool {
+    class TextPool {
+    public:
+        TextPool()= default; //domyslny konstruktor
+        TextPool(const std::initializer_list<std::experimental::string_view> &in) : poolset_(in) {}
 
-};
+        TextPool(TextPool &&pool); //konstruktor przenoszący
+        TextPool &operator=(TextPool &&pool); //operator przenoszący
+        TextPool(const TextPool &pool) = delete; //konstruktor kopiujący
+        TextPool &operator=(const TextPool &pool) = delete; //operator przypisania
+        ~TextPool()=default; //destruktor
 
+        std::experimental::string_view Intern(const std::string &str);
+        size_t StoredStringCount() const;
+        std::set<std::experimental::string_view> poolset_;
 
+    //private:
+
+    };
+
+}
 
 #endif //JIMP_EXERCISES_TEXTPOOL_H
