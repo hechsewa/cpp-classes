3fe9c9b2c10fb44ad47a19a779f2efe9a0dedaa8
Ewa Hechsman <ewa.hechsman@o2.pl>
2017-04-26 20:55:01 +0200

    fixing stuff

---
+++ lab6/point_op/Point.cpp
@@ -0,0 +1,53 @@
+//
+// Created by hushmans on 05.04.17.
+//
+
+#include "Point.h"
+#include <iomanip>
+#include <iostream>
+
+using ::std::istream;
+using ::std::ws;
+
+
+void Point::SetY(double y) { y_ = y; }
+void Point::SetX(double x) { x_ = x; }
+
+//Helper functions:
+void CheckNextChar(char c, istream* is) {
+    int next_char = is->peek();
+    if (next_char != c) {
+        throw std::runtime_error("invalid character");
+    }
+    is->ignore();
+}
+
+void IgnoreWhitespace(istream* is) {
+    (*is) >> ws;
+}
+
+double ReadNumber(istream* is) {
+    double d;
+    (*is) >> d;
+    return d;
+}
+
+// Właściwa definicja, obydwa argumenty funkcji nie
+//są zadeklarowane jako const, bo obydwa są modyfikowane
+//wewnątrz funkcji (STL nie używa naszej konwencji z przekazywaniem
+//przez wskaźnik)
+istream& operator>>(istream & input, Point& p){
+    CheckNextChar('(', &input);
+    p.SetX(ReadNumber(&input));
+    CheckNextChar(',', &input);
+    IgnoreWhitespace(&input);
+    p.SetY(ReadNumber(&input));
+    CheckNextChar(')', &input);
+    return input;      // Umożliwia cin >> a >> b >> c;
+}
+
+//operator wypisania
+std::ostream &operator<<(std::ostream &output, Point& p){
+    output << "(" << p.x_ << "," << p.y_ << ")";
+    return output;
+}
---
+++ lab6/point_op/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by hushmans on 05.04.17.
+//
+
+#include "Point.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    Point a;
+    a.SetX(16);
+    a.SetY(2);
+
+    cout<<a<<endl<<a;
+    return 0;
+}
