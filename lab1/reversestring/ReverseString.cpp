f80a21599c38d4294f8ead6a088ca19acdc67ab0
Ewa Hechsman <ewa.hechsman@o2.pl>
2017-05-02 22:54:48 +0200

    merging that reverse string for like 100th time

--- lab1/reversestring/ReverseString.cpp
+++ lab1/reversestring/ReverseString.cpp
@@ -1,13 +1,9 @@
 //
 // Created by mat on 04.03.17.
 //
-<<<<<<< .merge_file_sCVT33
 #include <ReverseString.h>
 #include <iostream>
 using namespace std;
-=======
-#include <reverse.h>
->>>>>>> .merge_file_SFmiv4
 
 std::string reverse(std::string str){
     if (str.length()==0 || str.length()==1) return str;
